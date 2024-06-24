214953f77d03f8c06cfead3961aa5861bd6e17a8
jessvasq <jessnsq.codes@gmail.com>
2024-06-23 22:34:50 -0400

    Home page

---
+++ Company_Site/src/main/java/com/nvz/company_site/CompanySiteApplication.java
@@ -0,0 +1,13 @@
+package com.nvz.company_site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanySiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanySiteApplication.class, args);
+    }
+
+}
---
+++ Company_Site/src/test/java/com/nvz/company_site/CompanySiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvz.company_site;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanySiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
